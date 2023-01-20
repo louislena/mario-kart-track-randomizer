5d4e047dc5c15127d39f5d9fbd85cd0e58cf446f
louislena <helenalouisesjostrand@gmail.com>
2023-01-20 20:13:57 +0100

    created full randomizer document

--- full_randomizer.py
+++ full_randomizer.py
@@ -1,49 +1,4 @@
-import random 
-
-character = []
-chosen_character = []
-
-with open("character.txt") as file:
-    for line in file: 
-            character.append(line.rstrip())
-
-chosen_character = random.sample(character, 1) 
-
-print(chosen_character)
-
-
-vehicle = []
-chosen_vehicle = []
-
-with open("vehicle.txt") as file:
-    for line in file: 
-            vehicle.append(line.rstrip())
-
-chosen_vehicle = random.sample(vehicle, 1) 
-
-print(chosen_vehicle)
-
-wheels = []
-chosen_wheels = []
-
-with open("wheels.txt") as file:
-    for line in file: 
-            wheels.append(line.rstrip())
-
-chosen_wheels = random.sample(wheels, 1) 
-
-print(chosen_wheels)
-
-gliders = []
-chosen_gliders = []
-
-with open("gliders.txt") as file:
-    for line in file: 
-            gliders.append(line.rstrip())
-
-chosen_gliders = random.sample(gliders, 1) 
-
-print(chosen_gliders)
+import random
 
 item = []
 
@@ -92,4 +47,47 @@ for i in range(6):
             myfile.write(current_track + ",")
 
 
+character = []
+chosen_character = []
 
+with open("character.txt") as file:
+    for line in file: 
+            character.append(line.rstrip())
+
+chosen_character = random.sample(character, 1) 
+
+print(chosen_character)
+
+
+vehicle = []
+chosen_vehicle = []
+
+with open("vehicle.txt") as file:
+    for line in file: 
+            vehicle.append(line.rstrip())
+
+chosen_vehicle = random.sample(vehicle, 1) 
+
+print(chosen_vehicle)
+
+wheels = []
+chosen_wheels = []
+
+with open("wheels.txt") as file:
+    for line in file: 
+            wheels.append(line.rstrip())
+
+chosen_wheels = random.sample(wheels, 1) 
+
+print(chosen_wheels)
+
+gliders = []
+chosen_gliders = []
+
+with open("gliders.txt") as file:
+    for line in file: 
+            gliders.append(line.rstrip())
+
+chosen_gliders = random.sample(gliders, 1) 
+
+print(chosen_gliders)
