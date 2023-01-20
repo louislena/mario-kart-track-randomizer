af0b13b6602600b742c5f113e5d44297d6c054f6
louislena <helenalouisesjostrand@gmail.com>
2023-01-20 20:21:21 +0100

    organization

--- code_full_randomizer.py
+++ code_full_randomizer.py
@@ -1,5 +1,50 @@
 import random
 
+character = []
+chosen_character = []
+
+with open("character.txt") as file:
+    for line in file: 
+            character.append(line.rstrip())
+
+chosen_character = random.sample(character, 1) 
+
+print(chosen_character)
+
+
+vehicle = []
+chosen_vehicle = []
+
+with open("vehicle.txt") as file:
+    for line in file: 
+            vehicle.append(line.rstrip())
+
+chosen_vehicle = random.sample(vehicle, 1) 
+
+print(chosen_vehicle)
+
+wheels = []
+chosen_wheels = []
+
+with open("wheels.txt") as file:
+    for line in file: 
+            wheels.append(line.rstrip())
+
+chosen_wheels = random.sample(wheels, 1) 
+
+print(chosen_wheels)
+
+gliders = []
+chosen_gliders = []
+
+with open("gliders.txt") as file:
+    for line in file: 
+            gliders.append(line.rstrip())
+
+chosen_gliders = random.sample(gliders, 1) 
+
+print(chosen_gliders)
+
 item = []
 
 with open("item_list.txt") as file:
@@ -45,49 +90,3 @@ for i in range(6):
 
         with open("played_tracks.txt", "a") as myfile:
             myfile.write(current_track + ",")
-
-
-character = []
-chosen_character = []
-
-with open("character.txt") as file:
-    for line in file: 
-            character.append(line.rstrip())
-
-chosen_character = random.sample(character, 1) 
-
-print(chosen_character)
-
-
-vehicle = []
-chosen_vehicle = []
-
-with open("vehicle.txt") as file:
-    for line in file: 
-            vehicle.append(line.rstrip())
-
-chosen_vehicle = random.sample(vehicle, 1) 
-
-print(chosen_vehicle)
-
-wheels = []
-chosen_wheels = []
-
-with open("wheels.txt") as file:
-    for line in file: 
-            wheels.append(line.rstrip())
-
-chosen_wheels = random.sample(wheels, 1) 
-
-print(chosen_wheels)
-
-gliders = []
-chosen_gliders = []
-
-with open("gliders.txt") as file:
-    for line in file: 
-            gliders.append(line.rstrip())
-
-chosen_gliders = random.sample(gliders, 1) 
-
-print(chosen_gliders)
