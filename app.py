0be1a3ee86cb97076676786b00baba9f141f1ee8
AndreDyrstad <dyrstad.a@gmail.com>
2023-06-27 21:43:33 +0200

    Updated cors and setup amount

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 
 from flask import request
+from flask_cors import CORS, cross_origin
 from track_randomizer.code_track_randomizer import track_randomizer, reset_tracks
 
 from player_setup_randomizer.code_character_vehicle_wheels_gliders import player_setup
@@ -12,9 +13,11 @@ from flask import Flask
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
+CORS(app, support_credentials=True)
 api = Api(app)
 
 class RandomTracks(Resource):
+    @cross_origin(supports_credentials=True)
     def get(self):
         track_amount = request.args.get('track_amount')
         if track_amount:
@@ -22,18 +25,25 @@ class RandomTracks(Resource):
         return track_randomizer(6)
 
 class RandomItems(Resource):
+    @cross_origin(supports_credentials=True)
     def get(self):
         return item_randomizer()
 
 class RandomSetup(Resource):
+    @cross_origin(supports_credentials=True)
     def get(self):
+        setup_amount = request.args.get('setup_amount')
+        if setup_amount:
+            return player_setup(int(setup_amount))
         return player_setup(2)
 
 class ResetTracks(Resource):
+    @cross_origin(supports_credentials=True)
     def get(self):
         return reset_tracks()
 
 class FullRandomizer(Resource):
+    @cross_origin(supports_credentials=True)
     def get(self):
         return full_randomizer()
 
@@ -49,4 +59,4 @@ api.add_resource(FullRandomizer, '/random')
 
 
 if __name__ == "__main__":
-    app.run(port=8000)
+    app.run()
